e420f09aa4e10e67377d24e9a19c11ae219d58e2
Jozef Nagy <schkwve@gmail.com>
2024-03-24 20:24:45 +0100

    Removed global state, added contexts

--- nighterm.h
+++ nighterm.h
@@ -2,8 +2,8 @@
  * @brief This file exports public Nighterm API.
  */
 
-#ifndef NIGHTERM_H_
-#define NIGHTERM_H_
+#ifndef NIGHTERM_H
+#define NIGHTERM_H
 
 #include <stddef.h>
 #include <stdint.h>
@@ -11,7 +11,9 @@
 /**
  * @brief Amount of spaces a tab (or \t) represents.
  */
-#define INDENT_AMOUNT 4
+#ifndef NIGHTERM_INDENT_WIDTH
+#define NIGHTERM_INDENT_WIDTH 4
+#endif
 
 #define PSF_MODE 2
 
@@ -19,12 +21,6 @@
 #define PSF_MAGIC1 0xb5
 #define PSF_MAGIC2 0x4a
 #define PSF_MAGIC3 0x86
-
-#ifndef NIGHTERM_MAX_TERMINALS
-#define NIGHTERM_MAX_TERMINALS 4
-#endif
-
-#define NIGHTERM_MAX_RESOLUTION (1920 * 1080)
 
 /**
  * @brief Memory allocator function pointer.
@@ -48,15 +44,20 @@ typedef struct
   uint32_t length;
   uint32_t charSize;
   uint32_t height, width;
-} __attribute__((packed)) psf2Hdr;
+} __attribute__((packed)) psf2_header;
 
 /**
  * @brief Nighterm Terminal object.
  */
-struct nighterm_terminal
+struct nighterm_ctx
 {
-  char* name;
-  psf2Hdr font_header;
+  void *fb_addr;
+  uint64_t fb_width;
+  uint64_t fb_height;
+  uint64_t fb_bpp;
+  uint64_t fb_pitch;
+
+  psf2_header font_header;
   void* font_data;
 
   uint8_t cur_x;
@@ -65,14 +66,11 @@ struct nighterm_terminal
   uint32_t rows;
   uint32_t cols;
 
-#ifdef NIGHTERM_MALLOC_IS_AVAILABLE
-  uint32_t* backbuffer;
-#else
-  uint32_t backbuffer[1920 * (4 * 1080)];
-#endif
-
   uint32_t fg_color;
   uint32_t bg_color;
+
+  nighterm_malloc malloc;
+  nighterm_free free;
 };
 
 /**
@@ -94,29 +92,9 @@ enum nighterm_status
   NIGHTERM_SUCCESS = 0
 };
 
-/**
- * @brief Nighterm configuration
- */
-struct nighterm_config
-{
-  void* fb_addr;
-  uint64_t fb_width;
-  uint64_t fb_height;
-  uint64_t fb_pitch;
-  uint16_t fb_bpp;
-
-  /* we have only one terminal for now... */
-  struct nighterm_terminal terminals[NIGHTERM_MAX_TERMINALS];
-  uint8_t terminal_count;
-  uint8_t current_terminal;
-
-  /* malloc/free */
-  nighterm_malloc malloc;
-  nighterm_free free;
-};
-
 int
-nighterm_initialize(void* font,
+nighterm_initialize(struct nighterm_ctx *new,
+                    void* font,
                     void* framebuffer_addr,
                     uint64_t framebuffer_width,
                     uint64_t framebuffer_height,
@@ -125,7 +103,7 @@ nighterm_initialize(void* font,
                     nighterm_malloc custom_malloc,
                     nighterm_free custom_free);
 void
-nighterm_shutdown(void);
+nighterm_shutdown(struct nighterm_ctx *context);
 
 int
 nighterm_create_terminal(char* name, char* font, uint8_t should_switch);
@@ -133,23 +111,23 @@ void
 nighterm_switch_terminal(int id);
 
 int
-nighterm_set_font(void *font);
+nighterm_set_font(struct nighterm_ctx *context, void *font);
 
 void
-nighterm_write(char c);
+nighterm_write(struct nighterm_ctx *context, char c);
 void
-nighterm_flush(uint8_t r, uint8_t g, uint8_t b);
+nighterm_flush(struct nighterm_ctx *context, uint8_t r, uint8_t g, uint8_t b);
 
 void
-nighterm_set_fg_color(uint8_t r, uint8_t g, uint8_t b);
+nighterm_set_fg_color(struct nighterm_ctx *context, uint8_t r, uint8_t g, uint8_t b);
 void
-nighterm_set_bg_color(uint8_t r, uint8_t g, uint8_t b);
+nighterm_set_bg_color(struct nighterm_ctx *context, uint8_t r, uint8_t g, uint8_t b);
 
 void
-nighterm_move_cursor(int32_t x, int32_t y);
+nighterm_move_cursor(struct nighterm_ctx *context, int32_t x, int32_t y);
 void
-nighterm_set_cursor_position(uint32_t x, uint32_t y);
+nighterm_set_cursor_position(struct nighterm_ctx *context, uint32_t x, uint32_t y);
 void
-nighterm_get_cursor_position(uint32_t* x, uint32_t* y);
+nighterm_get_cursor_position(struct nighterm_ctx *context, uint32_t* x, uint32_t* y);
 
-#endif // NIGHTERM_H_
+#endif // NIGHTERM_H
